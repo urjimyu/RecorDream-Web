bfc4fc22472cbee5fac92da46bd3ea1e2399ba0c
urjimyu <92876819+urjimyu@users.noreply.github.com>
2024-04-26 17:40:15 +0900

    chore: init

--- src/assets/svg/PcBackgrHalfstars.tsx
+++ src/assets/svg/PcBackgrHalfstars.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgPcBackgrHalfstars = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 1920 1080" {...props}>
--- src/assets/svg/PcRecordream.tsx
+++ src/assets/svg/PcRecordream.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgPcRecordream = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 200 36" {...props}>
